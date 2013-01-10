4179cbb1bc5e22392b730403029396763950bee2
Nobutaka Ito <noitojp@gmail.com>
2013-01-10 18:33:23 +0900

    add create logic

--- src/ndbm.c
+++ src/ndbm.c
@@ -5,9 +5,47 @@
 #include <fcntl.h>
 #include <signal.h>
 #include <unistd.h>
+#include <pthread.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 #include "ndbm.h"
+
+#define _DFL_DIRSHIFT 5
+#define _DFL_SEGSHIFT 12
+#define _DFL_DIRSIZE (1<<_DFL_DIRSHIFT)
+#define _DFL_SEGSIZE (1<<_DFL_SEGSHIFT)
+#define _DFL_MAX_BUCKETSIZE (_DFL_DIRSIZE * _DFL_SEGSIZE)
+#define _DFL_FFACTOR 4
+
+#define _NOW_VERSION 1
+#define _ALIGNMENT_BYTE 8
+#define _MAGIC "_ndbm_"
+#define _MAGIC_SIZ ((size_t)8)
+#define _SMALL_MSEG_NUM 64
+#define _SMALL_MSEG_NMEMB 8
+#define _BIG_MSEG_NUM 20
+#define _BIG_MSEG_NMEMB 4
+#define _SEGSIZE_THRESHOLD 1024
+#define _MAX_ALLOCSIZE (1024*1024*1024)
+
+typedef struct{
+	char magic[_MAGIC_SIZ];
+	pthread_spinlock_t lock_point;
+	int64_t free_offset;
+	int64_t small_msegs[_SMALL_MSEG_NUM];
+	int64_t big_msegs[_BIG_MSEG_NUM];
+	int32_t replaced;
+	int32_t version;
+	time_t ctime;
+//	int64_t dir[_DATA_HASH_SIZ];
+	int32_t dir_size;
+	int32_t seg_size;
+	int32_t dir_num;
+	int32_t seg_num;
+	int32_t bucket_num;
+	int64_t dir[];
+} _ndbm_header_t;
 
 static NDBM* _ndbm_create(const char *path,size_t size,int flags,mode_t mode);
 static NDBM* _ndbm_open(const char *path,int flags);
@@ -46,6 +84,8 @@ static NDBM* _ndbm_create(const char *path,size_t size,int flags,mode_t mode)
 {
 	NDBM *dbm = NULL;
 	int prot = PROT_READ;
+	_ndbm_header_t *header;
+	int ix;
 
 	if( NULL == path ){
 		goto error;
@@ -74,6 +114,32 @@ static NDBM* _ndbm_create(const char *path,size_t size,int flags,mode_t mode)
 	dbm->_base = mmap(0,dbm->_size,prot,MAP_SHARED,dbm->_fd,(off_t)0);
 	if( MAP_FAILED == dbm->_base ){
 		goto error;
+	}
+
+	header = (_ndbm_header_t*)dbm->_base;
+	strncpy(header->magic,_MAGIC,_MAGIC_SIZ);
+	header->version = _NOW_VERSION;
+	header->replaced = 0;
+	header->ctime = time(NULL);
+	header->dir_size = _DFL_DIRSIZE;
+	header->dir_num = 0;
+	header->seg_num = 0;
+	header->bucket_num = 0;
+	if( pthread_spin_init(&header->lock_point,PTHREAD_PROCESS_SHARED) != 0 ){
+		goto error;
+	}
+	header->free_offset = (int64_t)(sizeof(_ndbm_header_t) + sizeof(int64_t) * header->dir_size);
+
+	for(ix = 0; ix < _SMALL_MSEG_NUM; ix++ ){
+		header->small_msegs[ix] = (int64_t)-1;
+	}
+
+	for(ix = 0; ix < _BIG_MSEG_NUM; ix++ ){
+		header->big_msegs[ix] = (int64_t)-1;
+	}
+
+	for(ix = 0; ix < header->dir_size; ix++ ){
+		header->dir[ix] = (int64_t)-1;
 	}
 
 	return(dbm);
--- src/ndbm.h
+++ src/ndbm.h
@@ -19,14 +19,6 @@
 //#define vos_assign(cast,ptr) ((cast)(ptr? (char*)ptr-(char*)_nshm_base: -1))
 //#define vos_ptr(cast,offset) ((cast)(offset>0? (char*)_nshm_base+offset: 0))
 
-//typedef struct{
-//	int _fd;
-//	size_t _size;
-//	void* _base;
-//	sigset_t _full_set;
-//	sigset_t _backup_set;
-//} NShm;
-
 typedef struct{
 	int _fd;
 	size_t _size;
@@ -35,17 +27,13 @@ typedef struct{
 	sigset_t _backup_set;
 } NDBM;
 
-
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 extern NDBM* ndbm_open(const char *path,size_t size,int flags,mode_t mode);
 extern int ndbm_close(NDBM *dbm);
-//extern NShm* nshm_attach(const char *path);
-//extern void nshm_detach(NShm *nshm);
-//extern NShm* nshm_reattach(const char *path,NShm *nshm);
-//
+
 //extern void* nshm_memalloc(NShm *nshm,int32_t size);
 //extern void nshm_memfree(NShm *nshm,void *ptr);
 //extern void* shmalloc(NShm *nshm,int32_t size);
